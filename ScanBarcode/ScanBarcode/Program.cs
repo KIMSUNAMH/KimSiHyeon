cc05318cb15423e13c4ba100863746081fab216d
kimsh <kimsh@tascorp.co.kr>
2024-07-15 09:55:50 +0900

    데브익스 프레스 스킨 적용

---
+++ ScanBarcode/ScanBarcode/Program.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanBarcode
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            SkinManager.EnableFormSkins();
+            UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
+            Application.Run(new FormLogin());
+            Application.Run(new Form1());
+            //Application.Run(new MessageBoxForm());
+        }
+    }
+}
